6932bbdbd2e3b9ea4a6c44567abaa488adc334c4
RonaldVisitacion20 <ronald.visitacion@apuestatotal.com>
2023-03-17 16:33:42 -0500

    mi cambio desde prueba

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ echo "La fecha y hora actual es: " . date("Y-m-d H:i:s");
 echo "cambios de estado en rama master ";
 
 $variable="peimer cambio a master";
-echo $variable;
+// echo $variable;
+return $variable;
 ?>
 
