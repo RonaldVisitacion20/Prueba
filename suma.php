bdd9531bedd657bdb7f3d5a865dd6472460e6179
RonaldVisitacion20 <ronald.visitacion@apuestatotal.com>
2023-03-22 00:14:55 -0500

    dddfd

--- suma.php
+++ suma.php
@@ -29,3 +29,21 @@ print_r($array);
 $array[] = 6;
 print_r($array);
 
+// Re-indexar:
+$array = array_values($array);
+$array[] = 7;
+print_r($array);
+
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+ini_set('html_errors', false);
+// Array simple:
+$array = array(1, 2);
+$count = count($array);
+for ($i = 0; $i < $count; $i++) {
+    echo "\nRevisando $i: \n";
+    echo "Mal: " . $array['$i'] . "\n";
+    echo "Bien: " . $array[$i] . "\n";
+    echo "Mal: {$array['$i']}\n";
+    echo "Bien: {$array[$i]}\n";
+}
