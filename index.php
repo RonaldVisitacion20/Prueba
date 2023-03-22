087dd61cb048e92d7d16900f65241febade81362
RonaldVisitacion20 <ronald.visitacion@apuestatotal.com>
2023-03-21 23:42:48 -0500

    agregado de suma

--- index.php
+++ index.php
@@ -11,10 +11,13 @@ echo "es lunes de chamba";
 $variable="peimer cambio a master";
 $variable=$variable."agredado ";
 echo $variable;
+echo "martes 21";
 $arreglo=array();// iniciamos continualoprueba 
 $arreglo["edad"] = 12;
 $arreglo["nombres"] = "bethoben";
 $arreglo['DNI'] = 981287128;
 $arreglo['RUC'] = 72934729718312;
+
+
 ?>
 
