c79c185ea0be85ec4a8f15645cd2b03a2606614b
RonaldVisitacion20 <ronald.visitacion@apuestatotal.com>
2023-03-22 00:07:09 -0500

    33

--- suma.php
+++ suma.php
@@ -30,3 +30,17 @@ print_r($array);
 $array = array_values($array);
 $array[] = 7;
 print_r($array);
+
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+ini_set('html_errors', false);
+// Array simple:
+$array = array(1, 2);
+$count = count($array);
+for ($i = 0; $i < $count; $i++) {
+    echo "\nRevisando $i: \n";
+    echo "Mal: " . $array['$i'] . "\n";
+    echo "Bien: " . $array[$i] . "\n";
+    echo "Mal: {$array['$i']}\n";
+    echo "Bien: {$array[$i]}\n";
+}
