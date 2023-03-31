57bd154f1f94a920dbdc8905dc504e4fa4c68333
RonaldVisitacion20 <126714497+RonaldVisitacion20@users.noreply.github.com>
2023-03-31 17:41:41 -0500

    rama

--- index.php
+++ index.php
@@ -29,7 +29,12 @@ echo "Él tomó algo de jugo de $jugo.".PHP_EOL;
 // Inválido. "s" es un carácter válido para un nombre de variable, pero la variable es $jugo.
 echo "Él tomó algo de jugo hecho de $jugos.";
 // Válido. Explícitamente especifica el final del nombre de la variable encerrándolo entre llaves:
-echo "Él tomó algo de jugo hecho de ${jugo}s."
+echo "Él tomó algo de jugo hecho de ${jugo}s.";
+
+
+
+$variables_fa = "indice de inflaicon";
+
 
 ?>
 
